1d0e3fd92e7ac0ca41279405472f344d5d3ffd7a
kimyoungwon <42983671+kimyoungwon@users.noreply.github.com>
2021-03-04 15:46:46 -0800

    Add files via upload

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -12,3 +12,185 @@ def funky_sum(a, b, mix):
         return b
     else:
         return (1 - mix) * a + mix * b
+
+
+def count_divisible_digits(n, m):
+    """
+    Returns the number of digits in n that are divisible by m.
+    If m is 0, then count_divisible_digits should return 0.
+
+    Takes two integer numbers n and m as an arguments.
+    """
+    total = 0
+    n = abs(n)
+    if m == 0:
+        return 0
+    else:
+        while n > 0:
+            digit = n % 10
+            if digit % m == 0:
+                total += 1
+                n = n // 10
+            else:
+                total += 0
+                n = n // 10
+        return total
+
+
+def is_relatively_prime(n, m):
+    """
+    Returns True if n and m are relatively prime to each other,
+    returning False otherwise.
+
+    Takes two integer numbers n and m as an arguments.
+    """
+    prime_list1 = []
+    prime_list2 = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            prime_list1.append(i)
+    for j in range(1, m+1):
+        if m % j == 0:
+            prime_list2.append(j)
+    prime_list3 = list(set(prime_list1).intersection(prime_list2))
+    return prime_list3 == [1]
+
+
+def travel(direction, x, y):
+    """
+    Returns a string that indicates the new position
+    after following the directions starting from the given x, y in the format '(x_new, y_new)'.
+
+    Takes a string of North (N), East (E), South (S), West (W) directions,
+    a starting location on a grid x, and a starting location on a grid y.
+    The directions string will use 'N' to indicate increasing the y-coordinate,
+    'E' to indicate increasing the x-coordinate, 'S' to indicate decreasing the y-coordinate,
+    and 'W' to indicate decrease the x-coordinate.
+    The case of the characters should be ignored.
+    Any characters that are not 'N', 'E', 'W', or 'S' (ignoring letter-casing) should be ignored.
+    """
+    direction = direction.lower()
+    for i in str(direction):
+        if i == 'e':
+            x = x + 1
+        elif i == 'n':
+            y = y + 1
+        elif i == 'w':
+            x = x - 1
+        elif i == 's':
+            y = y - 1
+        else:
+            None
+    return str((x, y))
+
+
+def swip_swap(source, c1, c2):
+    """
+    Returns the string source with all occurrences of c1 and c2 swapped.
+
+    Takes a string source and characters c1 and c2.
+    """
+    result = ""
+    for i in range(len(source)):
+        if source[i] == c1:
+            result += c2
+        elif source[i] == c2:
+            result += c1
+        else:
+            result += source[i]
+    return result
+
+
+def compress(string):
+    """
+    Returns a new string such that each character is followed by its count,
+    and any adjacent duplicate characters are removed (replaced by the single character).
+    Takes a string as an argument.
+    """
+    if len(string) == 0:
+        return string
+
+    string = list(string)
+    ls = []
+    before = string[0]
+    count = 1
+
+    for i in range(1, len(string)):
+        if string[i] == before:
+            count += 1
+        else:
+            ls.append(before)
+            ls.append(str(count))
+            before = string[i]
+            count = 1
+    ls.append(before)
+    ls.append(str(count))
+    return ''.join(ls)
+
+
+def longest_line_length(file_name):
+    """
+    Returns the length of the longest line in the file.
+
+    Takes a string file_name.
+    """
+    with open(file_name) as f:
+        ls = []
+        result = None
+        for line in f.readlines():
+            ls.append(len(line))
+            result = max(ls)
+        return result
+
+
+def longest_word(file_name):
+    """
+    Returns the longest word in the file with which line it appears on.
+
+    Takes a string file_name.
+    """
+    with open(file_name) as f:
+        lines = f.readlines()
+        if len(lines) > 0:
+            line_number = 0
+            word_number = 0
+            for line in lines:
+                words = line.split()
+                line_number += 1
+                for word in words:
+                    if len(word) > word_number:
+                        word_number = len(word)
+                        longest_word = word
+                        line_with_the_word = line_number
+            return (str(line_with_the_word) + ': ' + longest_word)
+        else:
+            return None
+
+
+def mode_digit(digits):
+    """
+    Returns the digit that appears most frequently in that number.
+    The given number may be positive or negative,
+    but the most frequent digit returned should always be non-negative.
+    If there is a tie for the most frequent digit,
+    the digit with the greatest value should be returned.
+
+    Takes an integer number n.
+    """
+    counts = [0] * 10
+    if digits == 0:
+        counts[0] += 1
+    else:
+        digits = abs(digits)
+        while digits > 0:
+            digit = digits % 10
+            counts[digit] += 1
+            digits //= 10
+    max_count = 1
+    result = 0
+    for i in range(len(counts)):
+        count = counts[i]
+        if (count >= max_count):
+            max_count = count
+            result = i
+    return result
--- hw1/hw1_test.py
+++ hw1/hw1_test.py
@@ -44,11 +44,157 @@ def test_funky_sum():
     assert_equals(2, hw1.funky_sum(1, 2, 2.1))
     assert_equals(1.99, hw1.funky_sum(1, 2, 0.99))
 
+def test_count_divisible_digits():
+    """
+    Tests the function count_divisible_digits
+    """
+    print('Testing count_divisible_digits')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals(4, hw1.count_divisible_digits(650899, 3))
+    assert_equals(1, hw1.count_divisible_digits(-204, 5))
+    assert_equals(0, hw1.count_divisible_digits(24, 5))
+    assert_equals(0, hw1.count_divisible_digits(1, 0))
+
+    # Additional two cases
+    assert_equals(2, hw1.count_divisible_digits(-100, 5))
+    assert_equals(0, hw1.count_divisible_digits(5332, 6))
+
+def test_is_relatively_prime():
+    """
+    Tests the function is_relatively_prime
+    """
+    print('Testing is_relatively_prime')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals(True, hw1.is_relatively_prime(12, 13))
+    assert_equals(False, hw1.is_relatively_prime(4, 24))
+    assert_equals(True, hw1.is_relatively_prime(5, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 1) )
+
+    # Additional two cases
+    assert_equals(False, hw1.is_relatively_prime(36, 24) )
+    assert_equals(True, hw1.is_relatively_prime(13, 27) )
+
+def test_travel():
+    """
+    Tests the function travel
+    """
+    print('Testing travel')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals('(-1, 4)', hw1.travel('NW!ewnW',1, 2))
+ 
+    # Additional two cases
+    assert_equals('(2, 3)', hw1.travel('NW', 3, 2))
+    assert_equals('(6, 3)', hw1.travel('eS', 5, 4))
+
+def test_swip_swap():
+    """
+    Tests the swip_swap
+    """
+    print('Testing swip_swap')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals('offbar', hw1.swip_swap('foobar', 'f', 'o'))
+    assert_equals('foocar', hw1.swip_swap('foobar', 'b', 'c'))
+    assert_equals('foobar', hw1.swip_swap('foobar', 'z', 'c'))
+
+    # Additional two cases
+    assert_equals('bootfall', hw1.swip_swap('football', 'f', 'b'))
+    assert_equals('faatboll', hw1.swip_swap('football', 'o', 'a'))
+
+def test_compress():
+    """
+    Tests the compress
+    """
+    print('Testing compress')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals('c1o17l1k1a1n1g1a1r1o2', hw1.compress('cooooooooooooooooolkangaroo'))
+    assert_equals('a3', hw1.compress('aaa'))
+    assert_equals('', hw1.compress(''))
+
+    # Additional two cases
+    assert_equals('b3', hw1.compress('bbb'))
+    assert_equals('c3d3', hw1.compress('cccddd'))
+
+def test_longest_line_length():
+    """
+    Tests the longest_line_length
+    """
+    print('Testing longest_line_length')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals(13, hw1.longest_line_length('poem.txt'))
+
+    # Additional two cases
+    assert_equals(14, hw1.longest_line_length('poem2.txt'))
+    assert_equals(34, hw1.longest_line_length('poem3.txt'))
+
+def test_longest_word():
+    """
+    Tests the longest_word
+    """
+    print('Testing longest_word')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals('3: shells', hw1.longest_word('poem.txt'))
+
+    # Additional two cases
+    assert_equals('6: seashells', hw1.longest_word('poem2.txt'))
+    assert_equals('7: sea-whale-shells', hw1.longest_word('poem3.txt'))
+
+def test_mode_digit():
+    """
+    Tests the mode_digit
+    """
+    print('Testing mode_digit')
+
+    # Notice that we have to start the function calls with "hw1." since
+    # they live in another file
+
+    # Cases from the made up "spec" for this problem
+    assert_equals(1, hw1.mode_digit(12121))
+    assert_equals(0, hw1.mode_digit(0))
+    assert_equals(2, hw1.mode_digit(-122))
+    assert_equals(2, hw1.mode_digit(1211232231))
+
+    # Additional two cases
+    assert_equals(6, hw1.mode_digit(-166666666))
+    assert_equals(9, hw1.mode_digit(13339999555222))
 
 def main():
     test_funky_sum()
-    # Make sure you add the calls to all of your other functions here!
-
-
+    test_count_divisible_digits()
+    test_is_relatively_prime()
+    test_swip_swap() 
+    test_compress()
+    test_longest_line_length()
+    test_longest_word()
+    test_mode_digit()
+    
 if __name__ == '__main__':
     main()
